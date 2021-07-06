fde0e4359393d7e03a46523d9cf9ab3f070546f5
大馬裕一 <ohbay@daibayuuichinoMacBook-Air.local>
2021-07-07 00:39:06 +0900

    UPD camera.py

--- pi/camera.py
+++ pi/camera.py
@@ -1,6 +1,15 @@
 import time
 import picamera
 from datetime import datetime
+from smb.SMBConnection import SMBConnection
+
+host="192.168.0.100"  #ip or domain name
+username="click"
+password="password"
+conn=SMBConnection(username,password,"","",use_ntlm_v2 = True)
+result = conn.connect(host, 445)
+print("login successful")
+
 
 with picamera.PiCamera() as camera:
  camera.resolution = (1024, 768)
