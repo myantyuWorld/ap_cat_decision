f9696de4939d5bf348eff33b9759223b14fcaceb
yuichi_develop <nepinepi984@gmail.com>
2021-07-11 19:24:56 +0900

    modify

--- pi/back/camera.py
+++ pi/back/camera.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import numpy as np
+import base64
+import time
+import picamera
+import os
+from datetime import datetime
+from smb.SMBConnection import SMBConnection
+
+app = Flask('flask-tesseract-api')
+CORS(app)
+
+def image_file_to_base64(file_path):
+    with open(file_path, "rb") as image_file:
+        data = base64.b64encode(image_file.read())
+
+    return data.decode('utf-8')
 
 @app.route('/')
 def index():
     name = "Hello World"
     return name
+
+@app.route('/image')
+def get_image():
+    return image_file_to_base64("./sample/25.png")
+
+@app.route('/shooting')
+def shooting():
+ with picamera.PiCamera() as camera:
+  camera.resolution = (640, 480)
+  camera.start_preview()
+  time.sleep(2)
+  timestr = datetime.now().strftime('%Y%m%d%H%M%S')
+  camera.capture(timestr+'.jpg')
+
+  return image_file_to_base64(timestr+'.jpg')
 
 @app.route('/good')
 def good():
