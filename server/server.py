d7e58603344fc0501fab98cd4e039e6970f7d8f3
ohbay <ohbay@nextvision.co.jp>
2021-09-16 21:49:12 +0900

    server.py modify

--- server/server.py
+++ server/server.py
@@ -1,11 +1,13 @@
 import glob
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json
 import base64
 from io import BytesIO
 
 app = Flask(__name__)
+CORS(app)
 
 IMAGE_PATH = "\image\\*"
 
@@ -29,13 +31,16 @@ def get_pictures():
     files = glob.glob(os.getcwd() + IMAGE_PATH)
 
     print(os.getcwd() + IMAGE_PATH)
+    idx = 1
     for f in files:
         print(f)
         with open(f, "rb") as img_file:
             img = {
+                "id": idx,
                 "filename" : f,
                 "img" : base64.b64encode(img_file.read()).decode('utf-8')
             }
+            idx = idx + 1
             fileList.append(img)
 
     return jsonify({"fileList" : fileList})
